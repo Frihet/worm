c797e698f87ee9da71a842266201336aabb21195
noosmolegi <noosmolegi>
2008-04-17 11:55:46 +0000

    /div4/div4b/Worm/trunk Moved half of the logic in EditableTable to Webwidgets

--- Widgets/Table.py
+++ Widgets/Table.py
@@ -23,43 +23,6 @@
 import Webwidgets
 import Worm.Utils, Worm.Model.Base, Worm.Widgets.Base, Worm.Widgets.RowsMod, math, sqlalchemy.sql, itertools, types
 
-
-class EditFunctionCell(Webwidgets.FunctionCell):
-    html_class = ['edit_function_col']
-
-    input_path = ['edit']
-
-    edit_function_titles = {'edit': 'Edit',
-                            'save': 'Save',
-                            'revert': 'Revert',
-                            'delete': 'Delete'}
-
-    def draw_edit_function(self, table, row_id, is_editing, active, output_options):
-        if is_editing:
-            functions = ('save', 'revert', 'delete')
-        else:
-            functions = ('edit', 'delete')
-            
-        res = ''
-        for function in functions:
-            res += self.draw_function(table,
-                                      row_id, row_id,
-                                      ['edit_function', function],
-                                      function,
-                                      self.edit_function_titles[function],
-                                      active, output_options)
-        return res
-    
-    def draw_table_cell(self, output_options, row, table, row_num, column_name, rowspan, colspan, first_level, last_level):
-        row_id = table.ww_filter.get_row_id(row)
-        return self.draw_edit_function(table, row_id,
-                                       row.ww_filter.is_editing(),
-                                       table.get_active(table.path + ['_', 'edit_function']),
-                                       output_options)
-
-EditFunctionCellInstance = EditFunctionCell()
-
-
 class ReadonlyTable(Webwidgets.Table, Worm.Widgets.RowsMod.RowsComposite, Worm.Widgets.Base.Widget):
     debug_queries = False
     debug_expand_info = False
@@ -74,118 +37,76 @@ class ReadonlyTable(Webwidgets.Table, Worm.Widgets.RowsMod.RowsComposite, Worm.W
         WwFilters = Webwidgets.Table.RowsFilters.WwFilters + ["StaticRowsFilter"]
 
 
-class Table(ReadonlyTable):
-    class RowsRowModelWrapper(ReadonlyTable.RowsRowModelWrapper):
-        WwFilters = ["EditingFilter"] + ReadonlyTable.RowsRowModelWrapper.WwFilters
+class Table(ReadonlyTable, Webwidgets.EditableTable):
+    class RowsRowModelWrapper(Webwidgets.EditableTable.RowsRowModelWrapper, ReadonlyTable.RowsRowModelWrapper):
+        WwFilters = ["EditingFilters"] + ReadonlyTable.RowsRowModelWrapper.WwFilters
+        
+        class EditingFilters(Webwidgets.EditableTable.RowsRowModelWrapper.EditingFilters):
+            WwFilters = ["SQLAlchemyEditingFilter"] + Webwidgets.EditableTable.RowsRowModelWrapper.EditingFilters.WwFilters
+            
+            class SQLAlchemyEditingFilter(Webwidgets.Filter):
+                def __init__(self, *arg, **kw):
+                    Webwidgets.Filter.__init__(self, *arg, **kw)
+                    object.__setattr__(self, 'edit_widgets', {})
+                    if self.is_new():
+                        self.edit()
 
-        class EditingFilter(Webwidgets.Filter):
-            def __init__(self, *arg, **kw):
-                Webwidgets.Filter.__init__(self, *arg, **kw)
-                object.__setattr__(self, 'edit_widgets', {})
-                if self.is_new():
-                    self.edit()
+                def is_new(self):
+                    return getattr(self, 'ww_is_new', False)
 
-            def is_new(self):
-                return getattr(self, 'ww_is_new', False)
+                def is_editing(self):
+                    return self.edit_widgets != {}
 
-            def is_editing(self):
-                return self.edit_widgets != {}
+                def edit(self):
+                    if self.edit_widgets: return
+                    self.edit_session = self.table.db_session.bind.Session()
+                    if self.is_new():
+                        self.new_version = self.edit_session.save_and_expire(self.object.ww_model)
+                    else:
+                        #### fixme ####
+                        # name = """SQLAlchemy: merge clashes with
+                        # many-to-many"""
+                        # description = """Uggly hack since merge does not
+                        # seem to work when you have many-to-many
+                        # relationships!!! This uggly hack works since the
+                        # object is never changed in the main session, so
+                        # loading it straight from the DB will give the
+                        # right attribute values."""
+                        #### end ####
+                        #self.new_version = self.edit_session.merge(self.object.ww_model)
+                        t = type(self.object.ww_model)
+                        self.new_version = self.edit_session.query(t).filter(t.id == self.object.ww_model.id)[0]
+                    self.edit_widgets = self.new_version.get_column_input_widget_instances(
+                        self.edit_session, self.table.session, self.table.win_id)
 
-            def edit(self):
-                if self.edit_widgets: return
-                self.edit_session = self.table.db_session.bind.Session()
-                if self.is_new():
-                    self.new_version = self.edit_session.save_and_expire(self.object.ww_model)
-                else:
-                    #### fixme ####
-                    # name = """SQLAlchemy: merge clashes with
-                    # many-to-many"""
-                    # description = """Uggly hack since merge does not
-                    # seem to work when you have many-to-many
-                    # relationships!!! This uggly hack works since the
-                    # object is never changed in the main session, so
-                    # loading it straight from the DB will give the
-                    # right attribute values."""
-                    #### end ####
-                    #self.new_version = self.edit_session.merge(self.object.ww_model)
-                    t = type(self.object.ww_model)
-                    self.new_version = self.edit_session.query(t).filter(t.id == self.object.ww_model.id)[0]
-                self.edit_widgets = self.new_version.get_column_input_widget_instances(
-                    self.edit_session, self.table.session, self.table.win_id)
-                
-            def revert(self):
-                self.edit_session.close()
-                self.edit_widgets = {}
-                if self.is_new():
-                    self.table.pre_rows.remove(self.object.ww_model)
-                    
-            def save(self):
-                self.edit_session.commit()
-                self.object.ww_model.expire()
-                self.table.ww_filter.reread()
-                self.object.ww_filter.revert()
-                self.new_version.ww_is_new = False
+                def revert(self):
+                    self.edit_session.close()
+                    self.edit_widgets = {}
+                    if self.is_new():
+                        self.table.pre_rows.remove(self.object.ww_model)
 
-            def delete(self):
-                if self.is_new():
-                    self.revert()
-                else:
-                    self.table.db_session.delete(self.object)
-                    self.table.db_session.commit()
+                def save(self):
+                    self.edit_session.commit()
+                    self.object.ww_model.expire()
                     self.table.ww_filter.reread()
+                    self.object.ww_filter.revert()
+                    self.new_version.ww_is_new = False
 
-            def __getattr__(self, name):
-                if name in self.edit_widgets:
-                    return self.edit_widgets[name]
-                return getattr(self.ww_filter, name)
+                def delete(self):
+                    if self.is_new():
+                        self.revert()
+                    else:
+                        self.table.db_session.delete(self.object)
+                        self.table.db_session.commit()
+                        self.table.ww_filter.reread()
 
-    def field_input_edit_group_function(self, path, string_value):
-        if string_value == '': return
-        if path[0] == "new":
-            self.pre_rows.append(self.DBModel(ww_is_new = True))
+                def __getattr__(self, name):
+                    if name in self.edit_widgets:
+                        return self.edit_widgets[name]
+                    return getattr(self.ww_filter, name)
 
-    def field_output_edit_group_function(self, path):
-        return []
-    
-    def draw_edit_group_function(self, config, output_options):
-        return True, self.draw_group_function(['edit_group_function', "new"],
-                                              "new",
-                                              "Add new",
-                                              output_options)
-
-    def get_active_edit_group_function(self, path):
-        return self.session.AccessManager(Webwidgets.Constants.EDIT, self.win_id, self.path + ['edit_group_function'] + path)
-
-    class RowsFilters(ReadonlyTable.RowsFilters):
-        class TableEditableFilter(Webwidgets.Filter):
-            def get_rows(self, all, output_options):
-                res = []
-                for row in self.ww_filter.get_rows(all, output_options):
-                    row.edit_function_col = EditFunctionCellInstance
-                    res.append(row)
-                return res
-
-            def field_input_edit_function(self, path, string_value):
-                if string_value == '': return
-                row = self.object.ww_filter.get_row_by_id(string_value)
-                function = path[0]	
-                if function == "edit":
-                    row.ww_filter.edit()
-                elif function == "revert":
-                    row.ww_filter.revert()
-                elif function == "save":
-                    row.ww_filter.save()
-                elif function == "delete":
-                    row.ww_filter.delete()
-                    
-            def field_output_edit_function(self, path):
-                return []
-
-            def get_active_edit_function(self, path):
-                return self.session.AccessManager(Webwidgets.Constants.EDIT, self.win_id, self.path + ['edit'] + path)
-
+    class RowsFilters(ReadonlyTable.RowsFilters, Webwidgets.EditableTable.RowsFilters):
         WwFilters = ["TableEditableFilter"] + ReadonlyTable.RowsFilters.WwFilters
-
 
 class ExpandableTable(Table, Webwidgets.ExpandableTable):
     class RowsFilters(Table.RowsFilters, Webwidgets.ExpandableTable.RowsFilters):
